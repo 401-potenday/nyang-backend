9f76eab8983e74605955a3f87facd633001604f3
kmss6905 <kmss69052@naver.com>
2024-01-27 01:38:21 +0900

    init project

---
+++ src/test/java/potenday/app/PotendayApplicationTests.java
@@ -0,0 +1,13 @@
+package potenday.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotendayApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
