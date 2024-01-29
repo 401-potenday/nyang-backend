0151593a70e8dfe773507dc4e6a880173f9c136c
kmss6905 <kmss69052@naver.com>
2024-01-30 02:52:53 +0900

    feat: MethodArgumentNotValidException 에러 발생시 처리하는 헨들러 메서드 추가

--- src/main/java/potenday/app/global/error/ErrorCode.java
+++ src/main/java/potenday/app/global/error/ErrorCode.java
@@ -1,5 +1,7 @@
 package potenday.app.global.error;
 
+import java.util.Arrays;
+import java.util.List;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
@@ -7,16 +9,32 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
   E001("E001", "에러 메시지 내용이 들어갑니다.",  HttpStatus.BAD_REQUEST),
 
-  // User
-  U001("U001", "이미 사용 중인 닉네임 입니다.", HttpStatus.CONFLICT);
+  // 유저 닉네임
+  U001("U001", "이미 사용 중인 닉네임 입니다.", HttpStatus.CONFLICT),
+  U002("U002", "닉네임은 3 ~ 15자 사이 이어야 합니다.", HttpStatus.BAD_REQUEST),
+  U003("U003", "닉네임에는 비어있을 수 없습니다.", HttpStatus.BAD_REQUEST),
+  U004("U004", "닉네임을 입력하세요", HttpStatus.BAD_REQUEST),
+  U005("U005", "한글과 영문 문자만 허용됩니다.", HttpStatus.BAD_REQUEST),
+
+
+  // 정의되지 않는 에러
+  X001("X001", "서버에 문제가 발생하였습니다. 관리자에게 연락해주세요", HttpStatus.INTERNAL_SERVER_ERROR);
 
   private final String code;
   private final String message;
   private final HttpStatus httpStatus;
 
+  private static final List<ErrorCode> errorCodes = Arrays.stream(ErrorCode.values()).toList();
+
   ErrorCode(String code, String message, HttpStatus httpStatus) {
     this.code = code;
     this.message = message;
     this.httpStatus = httpStatus;
+  }
+
+  public static ErrorCode findCode(String code) {
+    return errorCodes.stream().filter(errorCode -> errorCode.getCode().equals(code))
+        .findFirst()
+        .orElse(X001);
   }
 }
--- src/main/java/potenday/app/global/error/GlobalExceptionHandler.java
+++ src/main/java/potenday/app/global/error/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package potenday.app.global.error;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import potenday.app.api.common.ApiResponse;
@@ -17,9 +19,30 @@ public class GlobalExceptionHandler {
       PotendayException e
   ) {
     ErrorCode ec = e.getErrorCode();
-    log.info("[error code] errCode = {}, message = {}, status = {}, instance = {}",
+    log.info("[potenday error] errCode = {}, message = {}, status = {}, instance = {}",
         ec.getCode(), ec.getMessage(), ec.getHttpStatus().value(), req.getRequestURI());
     return ResponseEntity.status(ec.getHttpStatus()).body(ApiResponse.error(ErrorContent.from(ec)));
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ApiResponse<ErrorContent>> handleMethodArgumentNotValidException(
+      HttpServletRequest req,
+      MethodArgumentNotValidException e
+  ) {
+    FieldError fieldError = e.getFieldErrors().get(0);
+    ErrorCode ec = ErrorCode.findCode(fieldError.getDefaultMessage());
+
+    // print log
+    if (ec.getHttpStatus().is5xxServerError()) {
+      log.error("[uncaught error], errCode = {}, message = {}, status = {}, instance = {}",
+          ec.getCode(), ec.getMessage(), ec.getHttpStatus().value(), req.getRequestURI());
+    } else {
+      log.info("[external error], errCode = {}, message = {}, status = {}, instance = {}",
+          ec.getCode(), ec.getMessage(), ec.getHttpStatus().value(), req.getRequestURI());
+    }
+
+    return ResponseEntity.status(ec.getHttpStatus())
+        .body(ApiResponse.error(ErrorContent.from(ec)));
   }
 
 }
