1c4281d96e65a3c941b334c20cb253159c6898af
kmss6905 <kmss69052@naver.com>
2024-01-29 23:52:55 +0900

    feat: ErrorCode 에 HttpStatus 도 담길 수 있도록 수정

--- src/main/java/potenday/app/global/error/ErrorCode.java
+++ src/main/java/potenday/app/global/error/ErrorCode.java
@@ -1,16 +1,22 @@
 package potenday.app.global.error;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
-  E001("E001", "에러 메시지 내용이 들어갑니다.");
+  E001("E001", "에러 메시지 내용이 들어갑니다.",  HttpStatus.BAD_REQUEST),
+
+  // User
+  U001("U001", "이미 사용 중인 닉네임 입니다.", HttpStatus.CONFLICT);
 
   private final String code;
   private final String message;
+  private final HttpStatus httpStatus;
 
-  ErrorCode(String code, String message) {
+  ErrorCode(String code, String message, HttpStatus httpStatus) {
     this.code = code;
     this.message = message;
+    this.httpStatus = httpStatus;
   }
 }
