09c41583c014906f2ae941ad644d9cc960b26ece
kmss6905 <kmss69052@naver.com>
2024-01-30 20:13:30 +0900

    feat: UserController 닉네임 업데이트 추가

--- src/main/java/potenday/app/api/user/UserController.java
+++ src/main/java/potenday/app/api/user/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import potenday.app.api.common.ApiResponse;
 import potenday.app.api.validation.NicknameValidationSequence;
+import potenday.app.domain.auth.AppUser;
+import potenday.app.domain.auth.AuthenticationPrincipal;
 import potenday.app.domain.user.UserService;
 
 @RestController
@@ -24,5 +26,14 @@ public class UserController {
   ) {
     boolean isAvailable = userService.checkAvailableNickname(userNicknameRequest.getNickname());
     return ApiResponse.success(new NicknameAvailableResponse(isAvailable));
+  }
+
+  @PostMapping("/user/nickname")
+  public ApiResponse<RegisterNicknameResponse> registerNickname(
+      @AuthenticationPrincipal AppUser appUser,
+      @Validated(value = NicknameValidationSequence.class) @RequestBody UserNicknameRequest userNicknameRequest
+  ) {
+    String registeredNickname = userService.registerNickname(appUser, userNicknameRequest.toNickname());
+    return ApiResponse.success(new RegisterNicknameResponse(registeredNickname));
   }
 }
