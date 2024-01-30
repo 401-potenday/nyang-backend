131892674c0b305954e8572c4c4c0e5e3b298ab0
kmss6905 <kmss69052@naver.com>
2024-01-30 17:13:25 +0900

    feat: client redirect uri -> yml 변수로 뺌

--- src/main/java/potenday/app/api/AuthController.java
+++ src/main/java/potenday/app/api/AuthController.java
@@ -1,8 +1,11 @@
 package potenday.app.api;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import java.net.URI;
 import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +13,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import potenday.app.api.auth.LoginResponse;
 import potenday.app.api.auth.TokenRequest;
-import potenday.app.api.common.ApiResponse;
 import potenday.app.domain.auth.AuthenticationService;
 import potenday.app.oauth.OAuthClient;
 import potenday.app.oauth.OAuthMember;
@@ -23,6 +25,9 @@ public class AuthController {
   private final OAuthUri oAuthUri;
   private final OAuthClient oAuthClient;
   private final AuthenticationService authenticationService;
+
+  @Value("${client.redirect-url}")
+  private String recirectUri;
 
   public AuthController(OAuthUri oAuthUri, OAuthClient oAuthClient,
       AuthenticationService authenticationService) {
@@ -39,15 +44,31 @@ public class AuthController {
   }
 
   @GetMapping("/auth/{oauthProvider}/token")
-  public ApiResponse<LoginResponse> generateAccessRefreshToken(
+  public ResponseEntity<?> generateAccessRefreshToken(
       @PathVariable String oauthProvider,
-      final TokenRequest tokenRequest
+      final TokenRequest tokenRequest,
+      HttpServletResponse httpServletResponse
   ) {
     // 1. OAuth 로 부터 유저정보 가져오기
     OAuthMember oAuthMember = oAuthClient.findOAuthMember(tokenRequest);
 
     // 2. 토큰 생성
     LoginResponse loginResponse = authenticationService.generateToken(oAuthMember, oauthProvider);
-    return ApiResponse.success(loginResponse);
+
+    setCookie(httpServletResponse, loginResponse);
+    return ResponseEntity.status(302)
+        .location(URI.create(recirectUri))
+        .build();
+  }
+
+  private void setCookie(HttpServletResponse response, LoginResponse loginResponse) {
+    Cookie accessToken = new Cookie("accessToken", loginResponse.accessToken());
+    accessToken.setPath("/");
+
+    Cookie refreshToken = new Cookie("refreshToken", loginResponse.refreshToken());
+    refreshToken.setPath("/");
+
+    response.addCookie(accessToken);
+    response.addCookie(refreshToken);
   }
 }
