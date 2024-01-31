4e39f2a9d7a5692b28b592c6dd27cb6c584c7ef9
kmss6905 <kmss69052@naver.com>
2024-01-31 13:48:29 +0900

    feat: 지원하는 Oauth Provider 를 검증하도록 함

--- src/main/java/potenday/app/global/error/ErrorCode.java
+++ src/main/java/potenday/app/global/error/ErrorCode.java
@@ -17,6 +17,9 @@ public enum ErrorCode {
   U005("U005", "한글과 영문 문자만 허용됩니다.", HttpStatus.BAD_REQUEST),
   U006("U006", "존재하지 않는 유저입니다.", HttpStatus.NOT_FOUND),
 
+  // 로그인
+  L001("L001","지원하지 않는 OAuth Provider", HttpStatus.BAD_REQUEST),
+
 
   // 정의되지 않는 에러
   X001("X001", "서버에 문제가 발생하였습니다. 관리자에게 연락해주세요", HttpStatus.INTERNAL_SERVER_ERROR);
