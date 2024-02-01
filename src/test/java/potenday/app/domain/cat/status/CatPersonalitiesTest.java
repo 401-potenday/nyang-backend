e6e1ca4e56e9526dc32b2b2dc2b5104dd8c2d7e8
kmss6905 <kmss69052@naver.com>
2024-02-01 12:55:08 +0900

    test: CatPersonalities 테스트 코드 추가

---
+++ src/test/java/potenday/app/domain/cat/status/CatPersonalitiesTest.java
@@ -0,0 +1,35 @@
+package potenday.app.domain.cat.status;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CatPersonalitiesTest {
+
+  @Test
+  @DisplayName("String 성격 코드 리스트를 이용해 실제 성격 코드가 담신 CatPersonalities 를 생성한다. - 성공")
+  void create() {
+    CatPersonalities catPersonalities = CatPersonalities.of(List.of("LIKES_PEOPLE", "LOVES_TO_CUDDLE"));
+
+    assertThat(catPersonalities.getPersonalities().size()).isEqualTo(2);
+    assertThat(catPersonalities.getPersonalities()).contains(CatPersonality.LIKES_PEOPLE,
+        CatPersonality.LOVES_TO_CUDDLE);
+  }
+
+  @Test
+  @DisplayName("CatPersonalities 에 담긴 성격 코드 리스트를 이용해 성격 코드를 문자열로 변환한다. - 성공")
+  void getPersonalitiesAsString() {
+    // given
+    CatPersonalities catPersonalities = CatPersonalities.of(
+        List.of("LIKES_PEOPLE", "LOVES_TO_CUDDLE"));
+
+    // when
+    String personalitiesWithComma = catPersonalities.concatenateCatPersonalitiesWithComma();
+
+    // then
+    assertThat(personalitiesWithComma).isIn("LIKES_PEOPLE, LOVES_TO_CUDDLE", "LOVES_TO_CUDDLE, LIKES_PEOPLE");
+
+  }
+}
