5dd374c7763505be367a7847e5920f580bd3d5b5
kmss6905 <kmss69052@naver.com>
2024-02-02 14:00:49 +0900

    feat: CatFollow 엔티티 추가

---
+++ src/main/java/potenday/app/domain/cat/follow/CatFollow.java
@@ -0,0 +1,37 @@
+package potenday.app.domain.cat.follow;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import potenday.app.domain.BaseTimeEntity;
+
+@Getter
+@Entity
+@Table(name = "CAT_CONTENT_FOLLOW")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CatFollow extends BaseTimeEntity {
+
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "cat_content_id", nullable = false)
+  private long catContentId;
+
+  @Column(name = "userId", nullable = false)
+  private long userId;
+
+  @Builder
+  public CatFollow(long catContentId, long userId) {
+    this.catContentId = catContentId;
+    this.userId = userId;
+  }
+}
---
+++ src/main/java/potenday/app/domain/cat/follow/CatFollowRespository.java
@@ -0,0 +1,7 @@
+package potenday.app.domain.cat.follow;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CatFollowRespository extends JpaRepository<CatFollow, Long> {
+
+}
