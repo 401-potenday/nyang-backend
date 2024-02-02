0ef1256680879a8609a86b520a21f638d0190349
kmss6905 <kmss69052@naver.com>
2024-02-02 21:52:38 +0900

    feat: CatCommentLike 엔티티 추가,
    - CatCommentLikeId 복합키로 구성

---
+++ src/main/java/potenday/app/domain/cat/commentlikes/CatCommentLikeRepository.java
@@ -0,0 +1,8 @@
+package potenday.app.domain.cat.commentlikes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CatCommentLikeRepository extends JpaRepository<CatCommentLike, Long> {
+
+  boolean existsByCatCommentLikeId(CatCommentLikeId catCommentLikeId);
+}
