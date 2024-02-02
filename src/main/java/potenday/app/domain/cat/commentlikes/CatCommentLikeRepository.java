5f18fe60ef8848f6f366e6748f6476d383223109
kmss6905 <kmss69052@naver.com>
2024-02-02 23:17:24 +0900

    feat: repository delete 시 리턴값 void 로 수정

--- src/main/java/potenday/app/domain/cat/commentlikes/CatCommentLikeRepository.java
+++ src/main/java/potenday/app/domain/cat/commentlikes/CatCommentLikeRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface CatCommentLikeRepository extends JpaRepository<CatCommentLike, Long> {
 
   boolean existsByCatCommentLikeId(CatCommentLikeId catCommentLikeId);
-  boolean deleteByCatCommentLikeId(CatCommentLikeId catCommentLikeId);
+  void deleteByCatCommentLikeId(CatCommentLikeId catCommentLikeId);
 }
