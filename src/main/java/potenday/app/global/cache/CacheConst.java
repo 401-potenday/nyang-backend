36b6d37152ac1585995b6fba228f432596ee88f4
kmss6905 <kmss69052@naver.com>
2024-02-03 20:22:01 +0900

    feat: Spring 캐싱 설정 추가

---
+++ src/main/java/potenday/app/global/cache/CacheConst.java
@@ -0,0 +1,9 @@
+package potenday.app.global.cache;
+
+public class CacheConst {
+
+  public static final String CAT_CONTENT_FOLLOW_COUNT = "CAT_CONTENT_FOLLOW_COUNT";
+  public static final String CAT_CONTENT_COMMENT_LIKE_COUNT = "CAT_CONTENT_COMMENT_LIKE_COUNT";
+  public static final String CAT_CONTENT_COMMENTS_LIST_WITH_PAGE = "CAT_CONTENT_COMMENTS_LIST_WITH_PAGE";
+
+}
