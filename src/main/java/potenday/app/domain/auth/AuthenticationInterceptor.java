f99053f60f40a9880647102e9c7df73f4eeee980
김민식 <kmss69052@naver.com>
2024-06-24 01:13:36 +0900

    Chore: 메서드 Options 의 경우 인증 필터 바로 통과

--- src/main/java/potenday/app/domain/auth/AuthenticationInterceptor.java
+++ src/main/java/potenday/app/domain/auth/AuthenticationInterceptor.java
@@ -14,6 +14,9 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        if (request.getMethod().equalsIgnoreCase("OPTIONS")) {
+            return true;
+        }
         authenticationTokenService.checkAuthentication(request);
         return true;
     }
