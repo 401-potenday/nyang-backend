eb0d5f8dbe6dcb14c01a95902ad474ce92e2ccba
김민식 <kmss69052@naver.com>
2024-06-30 19:46:44 +0900

    Feat: CachingConfig dev,local 에서만 동작하도록 변경

--- src/main/java/potenday/app/global/config/CachingConfig.java
+++ src/main/java/potenday/app/global/config/CachingConfig.java
@@ -12,9 +12,11 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 @EnableCaching
+@Profile({"local", "dev"})
 public class CachingConfig {
 
   @Bean
