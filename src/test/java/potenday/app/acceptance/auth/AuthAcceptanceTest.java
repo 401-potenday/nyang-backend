e311ec3168cbeaf1eca183037ee4a8d4f18e160e
김민식 <kmss69052@naver.com>
2024-07-08 19:14:31 +0900

    Test: Logout 기능 인수 테스트 추가

--- src/test/java/potenday/app/acceptance/auth/AuthAcceptanceTest.java
+++ src/test/java/potenday/app/acceptance/auth/AuthAcceptanceTest.java
@@ -1,15 +1,27 @@
 package potenday.app.acceptance.auth;
 
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.*;
 import static org.hamcrest.Matchers.is;
 
 import io.restassured.http.ContentType;
+import java.time.Duration;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import potenday.app.acceptance.AcceptanceTest;
+import potenday.app.domain.auth.TokenProvider;
 
 @DisplayName("인증 인수테스트")
 public class AuthAcceptanceTest extends AcceptanceTest {
+
+  @Autowired
+  StringRedisTemplate redisTemplate;
+
+  @Autowired
+  TokenProvider tokenProvider;
 
   @Test
   @DisplayName("OAuth 로그인 URI 요청 - 성공")
@@ -31,5 +43,69 @@ public class AuthAcceptanceTest extends AcceptanceTest {
       .assertThat().body("result", is("SUCCESS"))
       .assertThat().body("data.provider", is("KAKAO"))
       .assertThat().body("data.oauthUri", is("https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=client-id-sample&redirect_uri=http://localhost:3000"));
+  }
+
+  @Test
+  @DisplayName("로그아웃 요청 - 성공")
+  void logoutSuccessTest() {
+    // given
+    final String refreshToken = tokenProvider.issueAccessToken(1L);
+    saveToken(refreshToken);
+    given()
+          .log()
+          .all()
+          .contentType(ContentType.JSON)
+
+    // when
+    .when()
+      .header("Authorization", String.format("Bearer %s", userToken(1L)))
+      .body(String.format("""
+        {
+          "refreshToken": "%s"
+        }
+        """, refreshToken))
+      .post("/auth/logout")
+
+    // then
+    .then()
+      .log().all()
+      .assertThat().statusCode(200)
+      .assertThat().body("result", is("SUCCESS"));
+  }
+
+  @Test
+  @DisplayName("다른 유저의 refreshToken 로그아웃 요청 시 예외 발생 - 실패")
+  void logoutFailAnotherUserRefreshTokenTest() {
+    // given
+    final String refreshToken = tokenProvider.issueAccessToken(1L);
+    saveToken(refreshToken);
+    given()
+              .log()
+              .all()
+              .contentType(ContentType.JSON)
+
+        // when
+        .when()
+          .header("Authorization", String.format("Bearer %s", userToken(2L)))
+          .body(String.format("""
+          {
+            "refreshToken": "%s"
+          }
+          """, refreshToken))
+          .post("/auth/logout")
+
+        // then
+        .then()
+          .log().all()
+          .assertThat().statusCode(401)
+          .assertThat().body("result", is("ERROR"));
+  }
+
+  private void saveToken(String refreshToken) {
+    redisTemplate.opsForValue().set(refreshToken, "1", Duration.ofSeconds(1000L));
+  }
+
+  private String userToken(long id) {
+    return tokenProvider.issueAccessToken(id);
   }
 }
